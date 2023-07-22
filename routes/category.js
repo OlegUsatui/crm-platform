eb6ca79e6303ac4069a0430ca257fdb66769bc95
oleg-usatyi <oleg_usatyi@ukr.net>
2023-07-23 00:42:16 +0300

    parcing user data

--- routes/category.js
+++ routes/category.js
@@ -7,6 +7,6 @@ routes.get('/', controller.getAll);
 routes.get('/:id', controller.getById);
 routes.delete('/:id', controller.remove);
 routes.post('/', controller.create);
-routes.post('/:id', controller.update);
+routes.patch('/:id', controller.update);
 
 module.exports = routes;
