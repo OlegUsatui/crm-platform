8c159af53dd5b47db8a05a43acecd1898de1b788
oleg-usatyi <oleg_usatyi@ukr.net>
2023-07-23 17:13:43 +0300

    create category api

--- controllers/category.js
+++ controllers/category.js
@@ -1,41 +1,63 @@
-
-const errorHandler = require('../utils/errorHandler')
+const Category = require("../models/Category");
+const errorHandler = require('../utils/errorHandler');
 
 module.exports.getAll = async function (req, res) {
     try {
-
+        const category = await Category.find({
+            user: req.user.id
+        });
+        res.status(200).json(category)
     } catch (err) {
         errorHandler(err, res)
     }
 }
 
-module.exports.getById = function (req, res) {
+module.exports.getById = async function (req, res) {
     try {
-
+        const category = await Category.find({
+            _id: req.params.id,
+            user: req.user.id
+        });
+        res.status(200).json(category)
     } catch (err) {
         errorHandler(err, res)
     }
 }
 
-module.exports.remove = function (req, res) {
+module.exports.remove = async function (req, res) {
     try {
-
+        await Category.remove({
+            _id: req.params.id
+        })
+        res.status(200).json({
+            message: 'Категория была удалена'
+        })
     } catch (err) {
         errorHandler(err, res)
     }
 }
 
-module.exports.create = function (req, res) {
+module.exports.create = async function (req, res) {
     try {
-
+        const category = await new Category({
+            name: req.body.name,
+            imgSrc: req.body.imgSrc,
+            user: req.user.id
+        }).save()
+        res.status(201).json(category);
     } catch (err) {
         errorHandler(err, res)
     }
 }
 
-module.exports.update = function (req, res) {
+module.exports.update = async function (req, res) {
     try {
-
+        const category = await Category.findOneAndUpdate(
+            {_id: req.params.id},
+            {$set: req.body},
+            {new: true}
+        )
+        res.status(200).json(category)
     } catch (err) {
         errorHandler(err, res)
     }
---
+++ models/Category.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const categorySchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    imgSrc: {
+        type: String,
+        required: true
+    },
+    user: {
+        ref: 'user',
+        type: Schema.Types.ObjectId
+    }
+})
+
+module.exports = mongoose.model('categories', categorySchema)
