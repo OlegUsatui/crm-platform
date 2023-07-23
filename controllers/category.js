468a0d26aae038bfa4a13adc055232152f133eb0
oleg-usatyi <oleg_usatyi@ukr.net>
2023-07-23 17:04:42 +0300

    created position api

--- controllers/category.js
+++ controllers/category.js
@@ -1,15 +1,42 @@
-module.exports.getAll = function (req, res) {
 
+const errorHandler = require('../utils/errorHandler')
+
+module.exports.getAll = async function (req, res) {
+    try {
+
+    } catch (err) {
+        errorHandler(err, res)
+    }
 }
+
 module.exports.getById = function (req, res) {
+    try {
 
+    } catch (err) {
+        errorHandler(err, res)
+    }
 }
+
 module.exports.remove = function (req, res) {
+    try {
 
+    } catch (err) {
+        errorHandler(err, res)
+    }
 }
+
 module.exports.create = function (req, res) {
+    try {
 
+    } catch (err) {
+        errorHandler(err, res)
+    }
 }
-module.exports.update = function (req, res) {
 
+module.exports.update = function (req, res) {
+    try {
+
+    } catch (err) {
+        errorHandler(err, res)
+    }
 }
--- controllers/position.js
+++ controllers/position.js
@@ -1,12 +1,54 @@
-module.exports.getByCategoryId = function (req, res) {
+const Position = require('../models/Position');
+const errorHandler = require('../utils/errorHandler');
 
+module.exports.getByCategoryId = async function (req, res) {
+    try {
+        const positions = await Position.find({
+            category: req.params.categoryId,
+            user: req.user.id
+        });
+        res.status(200).json(positions)
+    } catch (err) {
+        errorHandler(err, res)
+    }
 }
-module.exports.remove = function (req, res) {
 
+module.exports.remove = async function (req, res) {
+    try {
+        await Position.remove({
+            _id: req.params.id
+        })
+        res.status(200).json({
+            message: 'Позиция была удалена'
+        })
+    } catch (err) {
+        errorHandler(err, res)
+    }
 }
-module.exports.create = function (req, res) {
 
+module.exports.create = async function (req, res) {
+    try {
+        const position = await new Position({
+            name: req.body.name,
+            category: req.body.category,
+            cost: req.body.cost,
+            user: req.user.id
+        }).save()
+        res.status(201).json(position);
+    } catch (err) {
+        errorHandler(err, res)
+    }
 }
-module.exports.update = function (req, res) {
 
+module.exports.update = async function (req, res) {
+    try {
+        const position = await Position.findOneAndUpdate(
+            {_id: req.params.id},
+            {$set: req.body},
+            {new: true}
+        )
+        res.status(200).json(position)
+    } catch (err) {
+        errorHandler(err, res)
+    }
 }
