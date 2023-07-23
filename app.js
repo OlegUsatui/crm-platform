5f31dc21424ca2ba7c70fc9df3f6464e4f9abeac
oleg-usatyi <oleg_usatyi@ukr.net>
2023-07-23 12:19:53 +0300

    mongodb connection

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const authRoutes = require('./routes/auth');
 const analyticsRoutes = require('./routes/analytics');
 const categoryRoutes = require('./routes/category');
 const orderRoutes = require('./routes/order');
 const positionRoutes = require('./routes/position');
+const keys = require('./config/keys')
+
+mongoose.connect(keys.mongoDBUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => console.log('Успешное подключение к MongoDB'))
+    .catch((err) => console.error('Ошибка подключения к MongoDB:', err));
 
 const app = express();
 
 app.use(require('morgan')('dev'))
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(require('cors')())
 
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoDBUrl: 'mongodb://localhost:27017/my_crm_mongo_db'
+}
