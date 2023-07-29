282d6f24be33a567fd9781def411c62940b986a3
oleg-usatyi <oleg_usatyi@ukr.net>
2023-07-29 15:26:47 +0300

    upload images

--- client/src/app/categories-page/categories-form/categories-form.component.ts
+++ client/src/app/categories-page/categories-form/categories-form.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { of, switchMap } from 'rxjs';
 import { CategoriesService } from '../../shared/services/categories.service';
+import { MaterializeService } from '../../shared/classes/materialize.service';
+import { Category } from '../../shared/interfaces/categories.interfaces';
 
 @Component({
   selector: 'app-categories-form',
@@ -10,9 +12,12 @@ import { CategoriesService } from '../../shared/services/categories.service';
   styleUrls: ['./categories-form.component.scss']
 })
 export class CategoriesFormComponent implements OnInit {
+  @ViewChild('inputFile') fileInputRef!: ElementRef;
   form!: FormGroup;
+  image?: File;
+  imagePreview: string | ArrayBuffer | null = '';
   isEditMode = false;
-
+  category?: Category;
   constructor(private route: ActivatedRoute,
               private fb: FormBuilder,
               private categoriesService: CategoriesService) {
@@ -28,21 +33,58 @@ export class CategoriesFormComponent implements OnInit {
 
     this.route.params.pipe(switchMap(params => {
       if(params['id']) {
+        this.isEditMode = true;
         return this.categoriesService.getById(params['id'])
       }
       return of(null)
     }))
       .subscribe(category => {
         if(category) {
+          this.category = category;
           this.form.patchValue({
             name: category.name
-          })
+          });
+          this.imagePreview = category.imgSrc;
+          MaterializeService.updateTextInput();
         }
         this.form.enable();
       })
   }
 
   onSubmit() {
+    let obs$;
+    this.form.disable();
+    if(this.isEditMode) {
+      obs$ = this.categoriesService.update(this.category!._id as string , this.form.value.name, this.image)
+    } else {
+      obs$ = this.categoriesService.create(this.form.value.name, this.image)
+    }
 
+    obs$.subscribe(
+      category => {
+        this.category = category;
+        MaterializeService.toast('Изменения сохранены');
+        this.form.enable();
+      },
+      error => {
+        MaterializeService.toast(error.error.message);
+        this.form.enable();
+      }
+    )
+  }
+
+  triggerClick() {
+    this.fileInputRef.nativeElement.click();
+  }
+
+  onUploadImage(event: any) {
+    const file = event.target.files[0];
+    this.image = file;
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imagePreview = reader.result
+    }
+
+    reader.readAsDataURL(file)
   }
 }
--- client/src/app/shared/classes/materialize.service.ts
+++ client/src/app/shared/classes/materialize.service.ts
@@ -10,4 +10,8 @@ export class MaterializeService {
   static initFloatingButton(ref: ElementRef) {
     M.FloatingActionButton.init(ref.nativeElement)
   }
+
+  static updateTextInput() {
+    M.updateTextFields();
+  }
 }
