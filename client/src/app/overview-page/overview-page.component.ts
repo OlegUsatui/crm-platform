676df074c0072a5973281d2e2da7074ec01c4f63
oleg-usatyi <oleg_usatyi@ukr.net>
2023-08-24 13:03:47 +0300

    overview page

--- client/src/app/overview-page/overview-page.component.ts
+++ client/src/app/overview-page/overview-page.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { OverviewPage } from '../shared/interfaces/analytics';
+import { AnalyticsService } from '../shared/services/analytics.service';
 
 @Component({
   selector: 'app-overview-page',
   templateUrl: './overview-page.component.html',
   styleUrls: ['./overview-page.component.scss']
 })
-export class OverviewPageComponent {
+export class OverviewPageComponent implements OnInit {
 
+  data$!: Observable<OverviewPage>;
+  yesterday = new Date();
+
+  constructor(private analyticsService: AnalyticsService) {
+  }
+
+  ngOnInit(): void {
+    this.data$ = this.analyticsService.getOverview();
+    this.yesterday.setDate(this.yesterday.getDate() - 1)
+  }
 }
